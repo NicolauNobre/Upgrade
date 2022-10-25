af3d258fe615a5c6d5c3d5ac8d3e95f9fe8e5104
Nicolas Nobre <nicolaunobreb@gmail.com>
2022-10-25 18:29:23 -0300

    nada demais

--- src/pages/Editcadastro/index.js
+++ src/pages/Editcadastro/index.js
@@ -147,98 +147,117 @@ export default function Editcadastro(params) {
                 <Animatable.View animation="fadeInUp" style={styles.containerForm}>
                 <Text style={styles.msgerro}>{vcadaster}</Text>
 
-                <Text style={styles.title}>Telefone (DDD+número) *</Text>
+                <Text style={styles.titleTel}>Telefone (DDD+número) *</Text>
                 <TextInput
                     keyboardType="phone-pad"
                     placeholder="Telefone..."
                     onChangeText={setPhone}
-                    style={styles.TextSenha}
+                    style={styles.TextInput}
                 />
                 <Text style={styles.msgerro}>{vphone}</Text>
                 
-                <Text style={styles.title}>E-mail *</Text>
+                <Text style={styles.titleEmail}>E-mail *</Text>
                 <TextInput
                     keyboardType="email-address"
                     placeholder="E-mail..."
                     onChangeText={setEmail}
-                    style={styles.TextSenha}
+                    style={styles.TextInput}
                 />
                 <Text style={styles.msgerro}>{vemail}</Text>
             
                 <Text style={styles.Dados}>Endereço</Text>
-                <Text style={styles.title}>CEP (apenas os números) *</Text>
-                <TextInput
+          
+                <View style={{flexDirection: 'row' }}>
+                  <Text style={styles.titleCEP}>CEP (apenas números)</Text>
+                  <Text style={styles.title}>Estado</Text>
+                </View>
+
+                <View style={{flexDirection: 'row' }}>
+                  <TextInput
                     keyboardType="number-pad"
                     placeholder="CEP..."
                     onChangeText={setZip}
-                    style={styles.TextSenha}
-                />
-                <Text style={styles.msgerro}>{vzip}</Text>
-                <Text style={styles.title}>Estado *</Text>
+                    style={styles.textCEP}
+                  />
                   <View style={styles.pickercontainer}>
                     <Picker
-                        style={styles.TextSenha}
+                        style={styles.textEst}
                         selectedValue={country}
                         onValueChange={(itemValue, itemIndex) =>setCountry(itemValue)}
-                        itemStyle={styles.TextSenha}
-                    >
-                        <Picker.Item label="UF" value="" />
-                        <Picker.Item label="Acre" value="Acre" />
-                        <Picker.Item label="Alagoas" value="Alagoas" />
-                        <Picker.Item label="Amapá" value="Amapá" />
-                        <Picker.Item label="Amazonas" value="Amazonas" />
-                        <Picker.Item label="Bahia" value="Bahia" />
-                        <Picker.Item label="Ceara" value="Ceara" />
-                        <Picker.Item label="Distrito Federal" value="Distrito Federal" />
-                        <Picker.Item label="Espírito Santo" value="Espírito Santo" />
-                        <Picker.Item label="Goiás" value="Goiás" />
-                        <Picker.Item label="Maranhão" value="Maranhão" />
-                        <Picker.Item label="Mato Grosso" value="Mato Grosso" />
-                        <Picker.Item label="Mato Grosso do Sul" value="Mato Grosso do Sul" />
-                        <Picker.Item label="Minas Gerais" value="Minas Gerais" />
-                        <Picker.Item label="Pará" value="Pará" />
-                        <Picker.Item label="Paraíba" value="Paraíba" />
-                        <Picker.Item label="Paraná" value="Paraná" />
-                        <Picker.Item label="Pernambuco" value="Pernambuco" />
-                        <Picker.Item label="Piauí" value="Piauí" />
-                        <Picker.Item label="Rio de Janeiro" value="Rio de Janeiro" />
-                        <Picker.Item label="Rio Grande do Norte" value="Rio Grande do Norte" />
-                        <Picker.Item label="Rio Grande do Sul" value="Rio Grande do Sul" />
-                        <Picker.Item label="Rondônia" value="Rondônia" />
-                        <Picker.Item label="Roraima" value="Roraima" />
-                        <Picker.Item label="Santa Catarina" value="Santa Catarina" />
-                        <Picker.Item label="São Paulo" value="São Paulo" />
-                        <Picker.Item label="Sergipe" value="Sergipe" />
-                        <Picker.Item label="Tocantins" value="Tocantins" />
-                    </Picker>
-                  </View>
-                <Text style={styles.title}>Cidade *</Text>
+                        itemStyle={styles.textEst}
+                      >
+                        <Picker.Item label="UF" value=''/>
+                        <Picker.Item label="AC" value="Acre" />
+                        <Picker.Item label="AL" value="Alagoas" />
+                        <Picker.Item label="AP" value="Amapá" />
+                        <Picker.Item label="AM" value="Amazonas" />
+                        <Picker.Item label="BA" value="Bahia" />
+                        <Picker.Item label="CE" value="Ceara" />
+                        <Picker.Item label="DF" value="Distrito Federal" />
+                        <Picker.Item label="ES" value="Espírito Santo" />
+                        <Picker.Item label="GO" value="Goiás" />
+                        <Picker.Item label="MA" value="Maranhão" />
+                        <Picker.Item label="MG" value="Mato Grosso" />
+                        <Picker.Item label="MS" value="Mato Grosso do Sul" />
+                        <Picker.Item label="MG" value="Minas Gerais" />
+                        <Picker.Item label="PA" value="Pará" />
+                        <Picker.Item label="PB" value="Paraíba" />
+                        <Picker.Item label="PR" value="Paraná" />
+                        <Picker.Item label="PE" value="Pernambuco" />
+                        <Picker.Item label="PI" value="Piauí" />
+                        <Picker.Item label="RJ" value="Rio de Janeiro" />
+                        <Picker.Item label="RN" value="Rio Grande do Norte" />
+                        <Picker.Item label="RS" value="Rio Grande do Sul" />
+                        <Picker.Item label="RO" value="Rondônia" />
+                        <Picker.Item label="RR" value="Roraima" />
+                        <Picker.Item label="SC" value="Santa Catarina" />
+                        <Picker.Item label="SP" value="São Paulo" />
+                        <Picker.Item label="SE" value="Sergipe" />
+                        <Picker.Item label="TO" value="Tocantins" />
+                      </Picker>
+                    </View>
+                </View>
+
+                {/* <TextInput
+                  placeholder="Estado..."
+                  onChangeText={setCountry}
+                  style={styles.TextSenha}
+                /> */}
+
+                <Text style={styles.title}>Cidade</Text>
                 <TextInput
-                    placeholder="Cidade..."
-                    onChangeText={setCity}
-                    style={styles.TextSenha}
+                  placeholder="Cidade..."
+                  onChangeText={setCity}
+                  style={styles.textCity}
                 />
-                <Text style={styles.title}>Rua *</Text>
+                <Text style={styles.title}>Rua</Text>
                 <TextInput
-                    placeholder="Rua..."
-                    onChangeText={setStreet}
-                    style={styles.TextSenha}
+                  placeholder="Rua..."
+                  onChangeText={setStreet}
+                  style={styles.textRua}
                 />
-                <Text style={styles.title}>Número *</Text>
-                <TextInput
+
+                <View style={{flexDirection: 'row' }}>
+                  <Text style={styles.titleNumber}>Número</Text>
+                  <Text style={styles.titleComp}>Complemento</Text>
+                </View>
+
+                <View style={{flexDirection: 'row' }}>
+                  <TextInput
                     keyboardType="number-pad"
                     placeholder="Número..."
                     onChangeText={setAdress}
-                    style={styles.TextSenha}
-                />
-                <Text style={styles.title}>Complemento</Text>
-                <TextInput
+                    style={styles.textNumber}
+                  />
+                  <TextInput
                     placeholder="Complemento..."
                     onChangeText={setComplement}
-                    style={styles.TextSenha}
-                />
+                    style={styles.textComp}
+                  />
+                </View>
+
                 <TouchableOpacity style={styles.button} onPress={() => salvar()}>
-                    <Text style={styles.buttonText}>Salvar</Text>
+                  <Text style={styles.buttonText}>Salvar</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.buttonRegister}
                 onPress={() => navigation.goBack()}>
@@ -254,26 +273,13 @@ export default function Editcadastro(params) {
 const styles = StyleSheet.create({
   container:{
     flex:1.,
-    backgroundColor: '#1E1E1E'
-  },
-  msgerro:{
-    color:"red",
-    fontSize: 20,
-    alignSelf:'center',
-  },
-  title:{
-    color: '#FF7851',
-    fontSize: 16,
+    backgroundColor: '#1E1E1E',
+    paddingBottom: 15,
   },
   containerHeader:{
     marginBottom: '8%',
     marginTop: '14%',
     paddingStart: '5%',
-  },
-  message:{
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#FF7851' 
   },
   containerForm:{
     backgroundColor: '#1E1E1E',
@@ -283,12 +289,133 @@ const styles = StyleSheet.create({
     paddingStart: '5%',
     paddingEnd: '5%',
   },
+  msgerro:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'center',
+  },
+  msgerroHalf:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 60,
+  },
+  msgerroTel:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 98,
+  },
+  msgerroEmail:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 110,
+  },
+  msgerroSenha:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 45,
+  },
+  msgerroCSenha:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 80,
+  },
+  msgerroCEP:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 65,
+  },
+  msgerroEst:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 90,
+  },
+  msgerroNumber:{
+    color:"red",
+    fontSize: 16,
+    alignSelf:'flex-start',
+    marginLeft: 45,
+    width: 80
+  },
+  message:{
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FF7851' 
+  },
   title: {
     color: '#FF7851',
     fontSize: 16,
     marginTop: 10,
     marginLeft: 35,
     marginBottom: 5,
+    alignSelf: 'flex-start',
+    width: '41%'
+  },
+  titleTel: {
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginLeft: 50,
+    marginBottom: 5,
+    marginRight: 100,
+    alignSelf: 'flex-start'
+  },
+  titleEmail: {
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginLeft: 50,
+    marginBottom: 5,
+    width: '66%',
+    alignSelf: 'flex-start'
+  },
+  titleSenha: {
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginLeft: 35,
+    marginBottom: 5,
+    width: '39%'
+  },
+  titleData: {
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+    marginRight: 100,
+    alignSelf: 'flex-start'
+  },
+  titleCEP:{
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginLeft: 35,
+    marginBottom: 5,
+    alignSelf: 'flex-start',
+    width: '50%'
+  },
+  titleComp:{
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+    alignSelf: 'flex-start',
+    width: '50%'
+  },
+  titleNumber:{
+    color: '#FF7851',
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 5,
+    marginLeft: 35,
+    alignSelf: 'flex-start',
+    width: '28%'
   },
   input:{
     backgroundColor: '#FFFFFF',
@@ -303,7 +430,7 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 50,
     paddingVertical: 8,
-    marginTop: 14,
+    marginTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center'
@@ -311,13 +438,14 @@ const styles = StyleSheet.create({
   buttonText:{
     color: '#1E1E1E',
     fontSize: 18,
+    //fontFamily: 'Inter'
   },
   buttonRegister:{
     marginTop: 14,
     alignSelf: 'center'
   },
   registerText:{
-    color: '#FF7851'
+    color: 'white'
   },
   TextInput:{
     backgroundColor: 'white',
@@ -327,14 +455,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     textAlign: 'center'
   },
-  TextSenha:{
-    backgroundColor: 'white',
-    color: 'black',
-    borderRadius: 50,
-    width: '80%',
-    alignSelf: 'center',
-    textAlign: 'center',
-  },
+  
   Dados:{
     color: '#FFFFFF',
     fontSize: 25,
@@ -342,15 +463,140 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   Obg:{
-    color: '#FF7851',
+    color: 'white',
     fontSize: 15,
     alignSelf: 'center',
     marginTop: 15,
   },
+  datebutton:{
+    backgroundColor: 'white',
+    borderRadius: 50,
+    width: '20%',
+    height: 30,
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 23
+  },
+  
+  datetext:{
+    backgroundColor: 'white',
+    color: 'gray',
+    height: 30,
+    textAlignVertical: 'center',
+    borderRadius: 50,
+    width: '100%',
+    fontSize: 15,
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
   pickercontainer:{
     backgroundColor: 'white',
     borderRadius: 50,
-    width: '80%',
+    width: '41%',
     alignSelf: 'center',
+    alignItems: 'center',
+    marginLeft: 31
+  },
+  textNome:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '85%',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  textHalf:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '35%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  textTel:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '45%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 23,
+  },
+  textEmail:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '60%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  textSenha:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '39%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  TextSenha:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '50%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  textCEP:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '37%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  textEst:{
+    backgroundColor: 'white',
+    color: 'black',
+    width: '65%',
+    alignSelf: 'center',
+    textAlign: 'center'
+  },
+  textCity:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '85%',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  textRua:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '85%',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  textNumber:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '20%',
+    textAlign: 'center',
+    marginLeft: 31,
+  },
+  textComp:{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: 50,
+    width: '58%',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    marginLeft: 31,
   }
 })
