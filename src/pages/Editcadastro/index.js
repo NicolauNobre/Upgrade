6dc0a833404a9c5801b3f74ee3326c07f331a1ce
IuriDkill <iuri.andrade.mundstock@gmail.com>
2022-12-25 19:04:29 -0300

    wip

--- src/pages/Editcadastro/index.js
+++ src/pages/Editcadastro/index.js
@@ -102,6 +102,10 @@ export default function Editcadastro(params) {
         setAdress(adress.toString())
       }
 
+      if(complement){
+        setComplement(complement.toString())
+      }
+
       return !error
     }
 
